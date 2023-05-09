3a04223c7433d2cc5fdfdc85ceb44d3079e73a03
Vladimir <circool@mail.ru>
2023-05-09 16:46:56 +0300

    1

--- source.c
+++ source.c
@@ -10,14 +10,18 @@ static uint32_t layer_colors[4] = {
 
 // Обработчик события слоя
 layer_state_t layer_state_set_user(layer_state_t state) {
+  // Получаем текущий активный слой
   uint8_t layer = get_highest_layer(state);
+
   if (layer > 0) {
-    // Установка цвета светодиода в зависимости от активного слоя
+    // Устанавливаем цвет светодиода в зависимости от активного слоя
     set_led_color(layer_colors[layer]);
   } else {
-    // Если слой отключен, отключить светодиод
+    // Если слой отключен, отключаем светодиод
     set_led_color(0x000000);
   }
+
+  // Возвращаем состояние слоя
   return state;
 }
 
@@ -27,28 +31,31 @@ static uint8_t minus_press_count = 0;
 
 // Обработчик нажатия клавиши "-"
 void process_minus_press(uint16_t keycode, keyrecord_t *record) {
-  // Проверка, что клавиша была нажата
+  // Проверяем, что клавиша нажата
   if (record->event.pressed) {
     uint32_t current_time = timer_read();
-    // Проверка, что это не повторный вызов функции
+
+    // Проверяем, что это не повторный вызов функции
     if (current_time - last_minus_press_time > 1000) {
-      // Сброс счетчика
+      // Сбрасываем счетчик нажатий
       if (minus_press_count == 4) {
         minus_press_count = 1;
       } else {
         minus_press_count++;
       }
     }
+
     if (minus_press_count == 4) {
-      // Получение текущего языка
+      // Получаем текущий язык клавиатуры
       char *lang = get_keyboard_language();
-      // Генерация новой строки
+      // Генерируем новую строку из символов "-"
       char str[20];
       memset(str, '-', 20);
-      // Отправка строки в операционную систему (на примере MacOS)
+      // Отправляем строку в операционную систему (на примере MacOS)
       send_string_to_os(str, lang);
     }
-    // Сохранение времени нажатия
+
+    // Сохраняем время нажатия
     last_minus_press_time = current_time;
   }
 }
@@ -57,10 +64,13 @@ void process_minus_press(uint16_t keycode, keyrecord_t *record) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case KC_MINUS:
+      // Вызываем обработчик нажатия "-"
       process_minus_press(keycode, record);
       break;
     default:
       break;
   }
+
+  // Возвращаем значение true, чтобы означать, что нажатие было обработано
   return true;
 }
